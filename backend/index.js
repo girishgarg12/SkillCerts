cadb3890ee81aa463290bed801850fe35aad13dc
suraj vishwakarma <dev.surajv@gmail.com>
2025-12-19 23:43:07 +0530

    Add user routes and configure CORS; update dependencies and scripts

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import { connectDB } from './db/dbconnect.js';
+import userRouter from './routes/user.routes.js';
+
+
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
 connectDB();
 
 const PORT =process.env.PORT || 3000;
